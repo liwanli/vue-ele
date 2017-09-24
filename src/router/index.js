622d58f8fa26134f354913d668e2857b24105a59
liwanli <970603897@qq.com>
2017-09-24 11:38:34 +0800

    创建项目

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import Hello from '@/components/Hello'
+import NotFoundComponent from '@/components/NotFoundComponent'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+const router = new Router({
+	//mode:'history',
+	routes: [
+		{
+		  path: '/',
+		  name: 'Hello',
+		  component: Hello
+		},
+		{
+		  path: '*',
+		  name: 'NotFoundComponent',
+		  component: NotFoundComponent
+		}
+	]
 })
+console.log(router)
+//输出 router
+export default router;
