6e545913b14b028db31d84c6b323e14be57fe371
李万里 <liwanli@tourongjia.com>
2017-10-11 09:28:34 +0800

    create

---
+++ src/components/goods/goods.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="goods">
+      <div class="goods">
+        <div class="goods-inner">
+          <aside id="aside">
+            <div class="aside-inner">
+                this is aside!
+            </div>
+          </aside>
+          <article id="article">
+            <div class="article-inner">
+              this is article!
+              <p>
+                  {{count}}
+                  <button @click="inc()">+</button>
+                  <button @click="dec()">-</button>
+              </p>
+              <ul>
+                <li v-for="(item,index) in articles">{{item.title}}</li>
+              </ul>
+            </div>
+          </article>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'goods',
+  data () {
+    return {
+      msg: Object,
+      articles: Array(),
+      count:0
+    }
+  },
+  methods: {
+    inc () {
+      this.count++
+    },
+    dec () {
+      this.count--
+    }
+  },
+  mounted: function() {
+    this.$http.jsonp('https://api.douban.com/v2/movie/top250?count=10', {}, {
+        headers: {
+
+        },
+        emulateJSON: true
+    }).then(function(response) {
+        this.articles = response.data.subjects
+        console.log(this.articles)
+    }, function(error) {
+        console.log('error')
+    });
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ 
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="header">
+    <div class="header-inner">
+      I am header!
+    </div>
+    <div class="header-backgroundImg"><img src="http://static.galileo.xiaojukeji.com/static/tms/seller_avatar_256px.jpg" width="100%" height="100%" alt=""></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'header',
+  data () {
+    return {
+    }
+  },
+  methods: {
+  },
+  mounted: function() {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped  lang='stylus' rel="stylesheet/stylus">
+  @import '../../common/stylus/page/_header.styl'
+</style>
